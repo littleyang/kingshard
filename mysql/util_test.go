7acabf5c24469134a3455067bd030516a1f015a8
littleyang <littleyangqh@gmail.com>
2015-12-30 15:48:10 +0800

    change doc

--- mysql/util_test.go
+++ mysql/util_test.go
@@ -1,17 +1,3 @@
-// Copyright 2015 The kingshard Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License"): you may
-// not use this file except in compliance with the License. You may obtain
-// a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
 package mysql
 
 import (
@@ -29,7 +15,7 @@ func TestCalcPassword(t *testing.T) {
 		hex:
 			406a783d645f337a34323b73532459725329707c
 		pass:
-			kingshard
+
 		scramble:
 			fbc71db5ac3d7b51048d1a1d88c1677f34bcca11
 	*/
